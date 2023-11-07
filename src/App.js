0b1f3a0d468c21ff1320712bd05b2b44a63a8aff
james king <jamesasuelimen77@gmail.com>
2023-11-07 11:24:21 +0100

    project structure and top nav component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Topnav from './components/topnav';
+
+function App() {
+  return (
+    <div className="App">
+      <Topnav/>
+    </div>
+  );
+}
+
+export default App;
