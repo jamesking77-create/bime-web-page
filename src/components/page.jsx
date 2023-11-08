1acca4bbfd3c30ecc412fa6a3d9b79f72a18716d
Felizdujadin <ezeikefelix51@gmail.com>
2023-11-08 19:17:41 +0100

    code upload

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/page.css'
 import Hero from './hero';
+import WebBlocking from './webBlocking';
 
 const Page = () => {
   return (
     <div className='page-container'>
       <Hero/>
+      <WebBlocking />
     </div>
   );
 }
