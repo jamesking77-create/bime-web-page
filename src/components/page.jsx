3475aff3f57ac6e30bca8c82783ca06909798397
james king <jamesasuelimen77@gmail.com>
2023-11-13 13:04:33 +0100

    reviews

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import '../styles/page.css'
 import Hero from './hero';
 import WebBlocking from './webBlocking';
+import Preview from './preview';
+import Reviews from './reviews';
 
 const Page = () => {
   return (
     <div className='page-container'>
       <Hero/>
       <WebBlocking />
+      <Preview/>
+      <Reviews/>
     </div>
   );
 }
---
+++ src/components/reviews.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/reviews.css';
+
+const Reviews = () => {
+  return (
+    <div>
+      <h1>Reviews</h1>
+      <div className="reviews-box">
+        <div className="reviews"></div>
+        <div className="reviews"></div>
+        <div className="reviews"></div>
+        <div className="reviews"></div>
+        <div className="reviews"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Reviews;
