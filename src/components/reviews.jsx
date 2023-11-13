5341246b6645d74a8921c62d6b774b2bbbc93c2b
james king <jamesasuelimen77@gmail.com>
2023-11-13 14:55:39 +0100

    reviews update

--- src/components/reviews.jsx
+++ src/components/reviews.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/reviews.css';
 
 const Reviews = () => {
+  const [hoveredReviewText, setHoveredReviewText] = useState('');
+
+  const handleReviewHover = (text) => {
+    setHoveredReviewText(text);
+  };
+
   return (
     <div>
-      <h1>Reviews</h1>
+      <h1 style={{ color: '#fff' }}>Reviews</h1>
       <div className="reviews-box">
-        <div className="reviews"></div>
-        <div className="reviews"></div>
-        <div className="reviews"></div>
-        <div className="reviews"></div>
-        <div className="reviews"></div>
+        <div className="reviews" id='r1' onMouseEnter={() => handleReviewHover('Text for Review 1')}>
+          {/* Content for Review 1 */}
+        </div>
+        <div className="reviews" onMouseEnter={() => handleReviewHover('Text for Review 2')}>
+          {/* Content for Review 2 */}
+        </div>
+        <div className="reviews" onMouseEnter={() => handleReviewHover('Text for Review 3')}>
+          {/* Content for Review 3 */}
+        </div>
+        <div className="reviews" onMouseEnter={() => handleReviewHover('Text for Review 4')}>
+          {/* Content for Review 4 */}
+        </div>
+        <div className="reviews" onMouseEnter={() => handleReviewHover('Text for Review 5')}>
+          {/* Content for Review 5 */}
+        </div>
+      </div>
+      <div className="review-text-box">
+        {hoveredReviewText && <p>{hoveredReviewText}</p>}
       </div>
     </div>
   );
 }
 
 export default Reviews;
+
